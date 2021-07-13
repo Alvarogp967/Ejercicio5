fd000f9cf4a7998b98d5a80aa84cfaf4ee5a71c9
Alvaro <agalanteperez@gmail.com>
2021-07-13 11:37:00 +0200

    Initial commit

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int A = 1, B = 2, C = 3, D = 4;
+            Console.WriteLine($" Valor de A, B, C, D {A} , {B} , {C} , {D}");
+            B = C;
+            C = A;
+            A = D;
+            D = B;
+            Console.WriteLine($" Valor de A, B, C, D {A} , {B} , {C} , {D}");
+        }
+    }
+}
